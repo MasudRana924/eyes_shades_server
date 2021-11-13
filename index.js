c52117ebc74b05fe62c1af9f7a76d0f3e1a837e3
MasudRana924 <masud15-924@diu.edu.bd>
2021-11-13 03:59:47 -0600

    delete post api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { MongoClient, CURSOR_FLAGS } = require('mongodb');
-const ObjectId=require('mongodb').ObjectId
+const ObjectId = require('mongodb').ObjectId
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
@@ -25,11 +25,11 @@ async function run() {
 
         //  Data section
         // get api for all data 
-         app.get('/glasses', async (req, res) => {
+        app.get('/glasses', async (req, res) => {
             const cursor = glassesCollection.find({})
             const glasses = await cursor.toArray()
             res.send(glasses)
-            
+
         })
         //    get single data 
         app.get('/glass/:id', async (req, res) => {
@@ -37,7 +37,7 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const product = await glassesCollection.findOne(query)
             res.json(product)
-        }) 
+        })
         // get api for all data 
         app.get('/products', async (req, res) => {
             const cursor = glassesCollection.find({})
@@ -54,8 +54,8 @@ async function run() {
             res.send({
                 count, products
             })
-        }) 
-          
+        })
+
         // orders api section 
         // orders post 
         app.post('/orders', async (req, res) => {
@@ -63,9 +63,9 @@ async function run() {
             const result = await ordersCollection.insertOne(order)
             res.json(result)
         })
-        app.get('/myorders',async(req,res)=>{
-            const email=req.query.email
-            const query={email:email}   
+        app.get('/myorders', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
             const cursor = ordersCollection.find(query)
             const orders = await cursor.toArray()
             res.send(orders)
@@ -73,14 +73,14 @@ async function run() {
         })
 
         // Users section 
-         // post user 
-         app.post('/users', async (req, res) => {
+        // post user 
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.json(result)
         })
         // make admin 
-         app.put('/users/admin', async (req, res) => {
+        app.put('/users/admin', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
             const updateDoc = { $set: { role: 'admin' } };
@@ -98,9 +98,25 @@ async function run() {
             }
             res.json({ admin: isAdmin });
         })
+        //    post api 
+        app.post('/glasses', async (req, res) => {
+            const newGlass = req.body
+            const result = await glassesCollection.insertOne(newGlass)
+            console.log('got new user  ', req.body)
+            console.log('added user', result)
+            // res.send('hit the post')
+            res.json(result)
+        })
+        //   delete api 
+        app.delete('/myorders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.json(result)
+        })
+        // post product glasses
 
 
-           
 
     }
     finally {
